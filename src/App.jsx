13c16f432789b77284f01179a4e92f511c87f084
kari-osk <karinaosuka@gmail.com>
2022-10-26 10:32:11 -0300

    Alter style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
